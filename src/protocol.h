6e19122d8d8846cc1d6f251e021144ee3d6c3425
kashh <cashhcointech@gmail.com>
2017-01-05 22:25:36 +0530

    port shift to 62617

--- src/protocol.h
+++ src/protocol.h
@@ -18,7 +18,7 @@
 extern bool fTestNet;
 static inline unsigned short GetDefaultPort(const bool testnet = fTestNet)
 {
-    return testnet ? 22613 : 62613;
+    return testnet ? 22613 : 62617;
 }
 
 
